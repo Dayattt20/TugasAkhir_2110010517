be52add4a4f551bb98ae426fcd30e95a93395312
Dayattt20 <dayathero20@gmail.com>
2023-07-03 11:37:45 +0800

    Membuat projek,unit,dan desain data orang tua atau wali

---
+++ Unit4.pas
@@ -0,0 +1,53 @@
+unit Unit4;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, DB, ZAbstractRODataset, ZAbstractDataset, ZDataset,
+  ZAbstractConnection, ZConnection, Grids, DBGrids, StdCtrls, ExtCtrls;
+
+type
+  TForm4 = class(TForm)
+    lbl1: TLabel;
+    pnl1: TPanel;
+    lbl2: TLabel;
+    lbl3: TLabel;
+    lbl4: TLabel;
+    lbl5: TLabel;
+    lbl6: TLabel;
+    lbl7: TLabel;
+    lbl8: TLabel;
+    edt1: TEdit;
+    edt2: TEdit;
+    edt3: TEdit;
+    edt4: TEdit;
+    edt5: TEdit;
+    edt6: TEdit;
+    edt7: TEdit;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    btn6: TButton;
+    dbgrd1: TDBGrid;
+    con1: TZConnection;
+    zqry1: TZQuery;
+    ds1: TDataSource;
+    lbl9: TLabel;
+    edt8: TEdit;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form4: TForm4;
+
+implementation
+
+{$R *.dfm}
+
+end.
